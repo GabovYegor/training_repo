6f4afafa4654dbf7d3291b14c534a840d7108d1f
Egor <gsa74@yandex.ru>
2019-02-18 10:11:18 +0300

    for pull

---
+++ vizualization_graph_1/main.cpp
@@ -0,0 +1,45 @@
+#include "widget.h"
+#include <QApplication>
+#include <QFile>
+#include <QDebug>
+#include <QString>
+
+bool** createConnectionMatrix(char* file_name, int& count_nodes){
+    QString str;
+    bool** connection_matrix;
+    QFile in_file(file_name); // лежит в директории сборки
+    in_file.open(QIODevice::ReadOnly);
+    str = in_file.readLine();
+    count_nodes = str.toInt();
+    connection_matrix = new bool*[count_nodes];
+    for(int i = 0; i < count_nodes; ++i){
+        str = in_file.readLine();
+        connection_matrix[i] = new bool[count_nodes];
+        for(int j = 0, k = 0; j < str.length(); ++j){
+            if(str[j] == "1"){
+                connection_matrix[i][k] = true;
+                ++k;
+            }
+            if(str[j] == "0"){
+                connection_matrix[i][k] = false;
+                ++k;
+            }
+        }
+    }
+    in_file.close();
+    return connection_matrix;
+}
+
+int main(int argc, char *argv[]){
+    QApplication a(argc, argv);
+    srand(time(nullptr));
+    bool** connection_matrix;
+    int count_nodes = 0;
+    if(argc == 2)
+        connection_matrix = createConnectionMatrix(argv[1], count_nodes);
+    else
+        connection_matrix = createConnectionMatrix("100nodes.txt", count_nodes);
+    Widget w(nullptr, connection_matrix, count_nodes);
+    w.show();
+    return a.exec();
+}
